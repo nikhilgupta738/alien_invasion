0c3c38828bd029e7264ebdf905c7f816da1e9017
Nikhilbest <ng929439@gmail.com>
2023-11-11 22:23:52 +0530

    Started project.

---
+++ resize.py
@@ -0,0 +1,17 @@
+from PIL import Image
+
+# Open the original image
+image = Image.open('images/alien.bmp')
+
+# Define the new width and height for the smaller image
+new_width = image.width // 2
+new_height = image.height // 2
+
+# Resize the image
+smaller_image = image.resize((new_width, new_height))
+
+# Save the smaller image
+smaller_image.save('images/alien_small.bmp')
+
+# Close the original image
+image.close()
